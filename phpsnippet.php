1ff9aa19cdf4bfbbd6de1fef5e1f1df9069d1b7a
ounziw <ounziw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-26 07:18:51 +0000

    version 2. add get_page_link
    git-svn-id: https://plugins.svn.wordpress.org/php-snippets-for-theme-designer/trunk@659137 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- phpsnippet.php
+++ phpsnippet.php
@@ -3,9 +3,10 @@
 Plugin Name: php snippet for theme designers
 Plugin URI: http://plugin.php-web.net/wp/tema-dezaina-no-tame-no-phpsunipetto
 Description: This plugin diplays a is_page()/is_category()/is_tag() snippet for page/category/tag list. You can go to the page/category/tag list in the admin area, and you can copy one (and paste it to a theme file).
-Version: 1.0
+Version: 2.0
 Author: Fumito Mizuno
 Author URI: http://php-web.net/
+License: GPL ver.2 or later
 */
 
 function readonly_textarea( $data, $rows=3, $cols=30 ) {
@@ -19,26 +20,39 @@ function create_snippet_if( $condition, $id, $slug ) {
     $condition = esc_html( $condition );
     $id = esc_html( $id );
     $slug = esc_html( urldecode( $slug ) );
-    $snippetformat = '&lt;?php if ( %1$s ( \'%2$s\' ) ):/* %3$s */ ?&gt; ' . "\n\n" . '&lt;?php endif; /* %1$s( \'%2$s\' ) %3$s */ ?&gt;';
+    $snippetformat = '&lt;?php if ( %1$s ( \'%2$s\' ) ): // %3$s  ?&gt; ' . "\n\n" . '&lt;?php endif; // %1$s( \'%2$s\' ) %3$s  ?&gt;';
     return sprintf($snippetformat, $condition, $id, $slug);
 }
 
+function create_snippet_url( $func_name, $id, $slug ) {
+    $func_name = esc_html( $func_name );
+    $id = esc_html( $id );
+    $slug = esc_html( urldecode( $slug ) );
+    $snippetformat = '&lt;?php echo esc_url( %1$s ( \'%2$s\' ) ); // %3$s  ?&gt; ';
+    return sprintf($snippetformat, $func_name, $id, $slug);
+}
+
 function add_snippet_column( $defaults ) {
-       $defaults['phpsnippet'] = __('PHP snippet') ;
+       $defaults['linksnippet'] = __('URL') ;
+       $defaults['ifsnippet'] = __('IF') ;
        return $defaults;
 }
 add_filter('manage_pages_columns', 'add_snippet_column');
 function add_categories_snippet_column( $defaults ) {
-       $defaults = array_merge($defaults,array('phpsnippet' =>'phpsnippet')) ;
+       $defaults['ifsnippet'] = __('IF') ;
        return $defaults;
 }
 add_filter('manage_edit-category_columns', 'add_categories_snippet_column');
 add_filter('manage_edit-post_tag_columns', 'add_categories_snippet_column');
 
 function add_snippet_text($column_name, $id) {
-    if( $column_name == 'phpsnippet' ) {
+    if( $column_name == 'ifsnippet' ) {
         $pageinfo = get_page( $id );
         $output = create_snippet_if( 'is_page', $pageinfo->ID, $pageinfo->post_name );
+        print readonly_textarea( $output );
+    } elseif( $column_name == 'linksnippet' ) {
+        $pageinfo = get_page( $id );
+        $output = create_snippet_url( 'get_page_link', $pageinfo->ID, $pageinfo->post_name );
         print readonly_textarea( $output );
     }
 
---
+++ phpsnippettest.php
@@ -0,0 +1,33 @@
+<?php 
+
+class PhpSnippetTest extends PHPUnit_Framework_TestCase {
+
+  
+  function test_readonly_textarea_ok() {  	
+		$expected ='<textarea readonly rows="3" cols="30" onclick="this.focus();this.select()">abcde</textarea>';
+		$output = readonly_textarea( 'abcde' );
+        $this->assertEquals($expected, $output);
+  }
+  function test_readonly_textarea_escape() {  	
+		$expected ='<textarea readonly rows="3" cols="30" onclick="this.focus();this.select()">a&lt;b&gt;cde</textarea>';
+		$output = readonly_textarea( 'a<b>cde', '3<b>', '30<b>' );
+        $this->assertEquals($expected, $output);
+  }
+
+  function test_create_snippet_if() {  	
+		$expected = "&lt;?php if ( is_page ( '2' ) ): // sample-page  ?&gt; 
+
+&lt;?php endif; // is_page( '2' ) sample-page  ?&gt;";
+		$output = create_snippet_if( 'is_page', '2', 'sample-page' );
+        $this->assertEquals($expected, $output);
+  }
+
+  function test_create_snippet_if_escape() {  	
+		$expected = "&lt;?php if ( is_&lt;b&gt;page ( '2&lt;b&gt;' ) ): // sample-&lt;b&gt;page  ?&gt; 
+
+&lt;?php endif; // is_&lt;b&gt;page( '2&lt;b&gt;' ) sample-&lt;b&gt;page  ?&gt;";
+		$output = create_snippet_if( 'is_<b>page', '2<b>', 'sample-<b>page' );
+        $this->assertEquals($expected, $output);
+  }
+
+}
