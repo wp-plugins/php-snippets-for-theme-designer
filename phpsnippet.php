cb5141efaa1b0ed4e180b510e1e30ee361850d8a
ounziw <ounziw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 09:37:40 +0000

    add prefix for funciton name
    git-svn-id: https://plugins.svn.wordpress.org/php-snippets-for-theme-designer/trunk@793485 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- phpsnippet.php
+++ phpsnippet.php
@@ -3,20 +3,20 @@
 Plugin Name: php snippet for theme designers
 Plugin URI: http://plugin.php-web.net/wp/tema-dezaina-no-tame-no-phpsunipetto
 Description: This plugin diplays a is_page()/is_category()/is_tag() snippet for page/category/tag list. You can go to the page/category/tag list in the admin area, and you can copy one (and paste it to a theme file).
-Version: 3.1
+Version: 3.2
 Author: Fumito Mizuno
 Author URI: http://php-web.net/
 License: GPL ver.2 or later
 */
 
-function readonly_textarea( $data, $rows=3, $cols=30 ) {
+function psftd_readonly_textarea( $data, $rows=3, $cols=30 ) {
     $data = esc_html( $data );
     $rows = intval( $rows );
     $cols = intval( $cols );
     return '<textarea readonly rows="' . $rows . '" cols="' . $cols . '" onclick="this.focus();this.select()">' . $data . '</textarea>';
 }
 
-function create_snippet_if( $condition, $id, $slug ) {
+function psftd_create_snippet_if( $condition, $id, $slug ) {
     $condition = esc_html( $condition );
     $id = esc_html( $id );
     $slug = esc_html( urldecode( $slug ) );
@@ -24,7 +24,7 @@ function create_snippet_if( $condition, $id, $slug ) {
     return sprintf($snippetformat, $condition, $id, $slug);
 }
 
-function create_snippet_url( $func_name, $id, $slug ) {
+function psftd_create_snippet_url( $func_name, $id, $slug ) {
     $func_name = esc_html( $func_name );
     $id = esc_html( $id );
     $slug = esc_html( urldecode( $slug ) );
@@ -32,72 +32,72 @@ function create_snippet_url( $func_name, $id, $slug ) {
     return sprintf($snippetformat, $func_name, $id, $slug);
 }
 
-function add_snippet_column( $defaults ) {
+function psftd_add_snippet_column( $defaults ) {
        $defaults['linksnippet'] = __('URL') ;
        $defaults['ifsnippet'] = __('IF') ;
        return $defaults;
 }
-add_filter('manage_pages_columns', 'add_snippet_column');
-add_filter('manage_posts_columns', 'add_snippet_column');
-function add_categories_snippet_column( $defaults ) {
+add_filter('manage_pages_columns', 'psftd_add_snippet_column');
+add_filter('manage_posts_columns', 'psftd_add_snippet_column');
+function psftd_add_categories_snippet_column( $defaults ) {
        $defaults['ifsnippet'] = __('IF') ;
        $defaults['linksnippet'] = __('LINK') ;
        return $defaults;
 }
-add_filter('manage_edit-category_columns', 'add_categories_snippet_column');
-add_filter('manage_edit-post_tag_columns', 'add_categories_snippet_column');
+add_filter('manage_edit-category_columns', 'psftd_add_categories_snippet_column');
+add_filter('manage_edit-post_tag_columns', 'psftd_add_categories_snippet_column');
 
-function add_snippet_text($column_name, $id) {
+function psftd_add_snippet_text($column_name, $id) {
     if( $column_name == 'ifsnippet' ) {
         $pageinfo = get_page( $id );
 		$slug = get_post_type($pageinfo->ID) . ': ' . $pageinfo->post_name;
-        $output = create_snippet_if( 'is_page', $pageinfo->ID, $slug );
-        print readonly_textarea( $output );
+        $output = psftd_create_snippet_if( 'is_page', $pageinfo->ID, $slug );
+        print psftd_readonly_textarea( $output );
     } elseif( $column_name == 'linksnippet' ) {
         $pageinfo = get_page( $id );
 		$slug = get_post_type($pageinfo->ID) . ': ' . $pageinfo->post_name;
-        $output = create_snippet_url( 'get_permalink', $pageinfo->ID, $slug );
-        print readonly_textarea( $output );
+        $output = psftd_create_snippet_url( 'get_permalink', $pageinfo->ID, $slug );
+        print psftd_readonly_textarea( $output );
     }
 
 }
-add_action('manage_pages_custom_column', 'add_snippet_text', 10, 2);
-function add_post_snippet_text($column_name, $id) {
+add_action('manage_pages_custom_column', 'psftd_add_snippet_text', 10, 2);
+function psftd_add_post_snippet_text($column_name, $id) {
     if( $column_name == 'ifsnippet' ) {
         $pageinfo = get_page( $id );
 		$slug = get_post_type($pageinfo->ID) . ': ' . $pageinfo->post_name;
-        $output = create_snippet_if( 'is_single', $pageinfo->ID, $slug );
-        print readonly_textarea( $output );
+        $output = psftd_create_snippet_if( 'is_single', $pageinfo->ID, $slug );
+        print psftd_readonly_textarea( $output );
     } elseif( $column_name == 'linksnippet' ) {
         $pageinfo = get_page( $id );
 		$slug = get_post_type($pageinfo->ID) . ': ' . $pageinfo->post_name;
-        $output = create_snippet_url( 'get_permalink', $pageinfo->ID, $slug );
-        print readonly_textarea( $output );
+        $output = psftd_create_snippet_url( 'get_permalink', $pageinfo->ID, $slug );
+        print psftd_readonly_textarea( $output );
     }
 
 }
-add_action('manage_posts_custom_column', 'add_post_snippet_text', 10, 2);
-function add_categories_snippet_text($null,$column_name, $id) {
+add_action('manage_posts_custom_column', 'psftd_add_post_snippet_text', 10, 2);
+function psftd_add_categories_snippet_text($null,$column_name, $id) {
     if( $column_name == 'ifsnippet' ) {
         $cat_name = get_cat_name($id);
-        $output = create_snippet_if( 'is_category', $id, $cat_name );
-        return readonly_textarea($output);
+        $output = psftd_create_snippet_if( 'is_category', $id, $cat_name );
+        return psftd_readonly_textarea($output);
     } elseif( $column_name == 'linksnippet' ) {
         $cat_name = get_cat_name($id);
-        $output = create_snippet_url( 'get_category_link', $id, $cat_name );
-        print readonly_textarea( $output );
+        $output = psftd_create_snippet_url( 'get_category_link', $id, $cat_name );
+        print psftd_readonly_textarea( $output );
     }
 }
-add_filter('manage_category_custom_column', 'add_categories_snippet_text', 10, 3);
-function add_post_tag_snippet_text($null,$column_name, $id) {
+add_filter('manage_category_custom_column', 'psftd_add_categories_snippet_text', 10, 3);
+function psftd_add_post_tag_snippet_text($null,$column_name, $id) {
     if( $column_name == 'ifsnippet' ) {
         $tag_array = get_term_by('id',$id,'post_tag');
-        $output = create_snippet_if( 'is_tag', $id, $tag_array->name );
-        return readonly_textarea($output);
+        $output = psftd_create_snippet_if( 'is_tag', $id, $tag_array->name );
+        return psftd_readonly_textarea($output);
     } elseif( $column_name == 'linksnippet' ) {
         $tag_array = get_term_by('id',$id,'post_tag');
-        $output = create_snippet_url( 'get_tag_link', $id, $tag_array->name );
-        print readonly_textarea( $output );
+        $output = psftd_create_snippet_url( 'get_tag_link', $id, $tag_array->name );
+        print psftd_readonly_textarea( $output );
     }
 }
-add_filter('manage_post_tag_custom_column', 'add_post_tag_snippet_text', 10, 3);
+add_filter('manage_post_tag_custom_column', 'psftd_add_post_tag_snippet_text', 10, 3);
